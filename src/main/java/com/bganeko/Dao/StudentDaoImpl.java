b376eb2c2449a871872148a89233253a6905c973
Brandon Ganeko <bganeko@hawaii.edu>
2019-04-16 22:19:09 -1000

    Initial Commit: Created backend services for Spring Boot Application

---
+++ src/main/java/com/bganeko/Dao/StudentDaoImpl.java
@@ -0,0 +1,56 @@
+package com.bganeko.Dao;
+
+import com.bganeko.Entity.Student;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Collection;
+import java.util.Map;
+
+
+@Repository
+@Qualifier("fakeData")
+public class StudentDaoImpl implements StudentDao {
+
+    private static Map<Integer, Student> students;
+
+    static {
+        students = new HashMap<Integer, Student>(){
+
+                {
+                    put(1, new Student(1, "tom", "computer science"));
+                    put(2, new Student(2, "bob", "math"));
+                    put(3, new Student(3, "ron", "bio"));
+                }
+            };
+        }
+
+    @Override
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    }
+
+    @Override
+    public Student getStudentById(int id){
+        return this.students.get(id);
+    }
+
+    @Override
+    public void removeStudentById(int id) {
+        this.students.remove(id);
+    }
+
+    @Override
+    public void updateStudent(Student student){
+        Student s = students.get(student.getId());
+        s.setCourse(student.getCourse());
+        s.setName(student.getName());
+        students.put(student.getId(), student);
+    }
+
+    @Override
+    public void insertStudentToDb(Student student) {
+        this.students.put(student.getId(), student);
+    }
+}
